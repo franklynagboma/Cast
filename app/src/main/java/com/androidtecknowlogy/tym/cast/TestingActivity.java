792c48aa98f91ecf24db663db96ce386af4b6cc6
franklyn <Androidtecknowlogy@gmai.com>
2017-07-27 22:55:08 +0100

    Adding texting activity to event  branch.

---
+++ app/src/main/java/com/androidtecknowlogy/tym/cast/TestingActivity.java
@@ -0,0 +1,17 @@
+package com.androidtecknowlogy.tym.cast;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AGBOMA franklyn on 7/27/17.
+ */
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
