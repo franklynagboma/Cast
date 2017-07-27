1ae75bcb7ea6fa01a364e4f21c62401f88ee7b93
franklyn <Androidtecknowlogy@gmai.com>
2017-07-27 22:51:08 +0100

    Settings activity added to settings branch.

---
+++ app/src/main/java/com/androidtecknowlogy/tym/cast/settings/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.androidtecknowlogy.tym.cast.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by AGBOMA franklyn on 7/27/17.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
